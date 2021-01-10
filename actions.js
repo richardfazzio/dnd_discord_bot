0d00877df9e67f5c14656ff98d990fff2812fd72
unknown <rfazzio@fau.edu>
2021-01-10 18:13:06 -0500

    First commit

---
+++ actions.js
@@ -0,0 +1,54 @@
+const ACTIONS = {
+    ROLL_DICE: /^!roll\s*\d*$/,
+    ROLL_STAT: /^!roll stat$/,
+    ROLL_STATS: /^!roll stats\s*\d*$/
+};
+
+const DEFAULTS = {
+    DIE_ROLL: 20,
+    STAT_DIE: 6,
+    NUMBER_OF_DIE: 6
+};
+
+const handleResponse = (msg) => {
+	const { content } = msg;
+	if (ACTIONS.ROLL_DICE.test(content)) {
+		return rollDice(content);
+    } else if (ACTIONS.ROLL_STAT.test(content)) {
+		return rollStat(content);
+	} else if (ACTIONS.ROLL_STATS.test(content)) {
+		return rollStats(content);
+	}
+};
+
+const roll = (num) => {
+	return Math.ceil(Math.random() * num);
+};
+
+const rollStat = () => {
+    return roll(DEFAULTS.STAT_DIE) + roll(DEFAULTS.STAT_DIE) + roll(DEFAULTS.STAT_DIE);
+}
+
+// Returns a list of stats of length n
+const rollStats = (content) => {
+    let [action, stats, n] = content.split(' ');
+    n = n || DEFAULTS.NUMBER_OF_DIE;
+    const results = [];
+    for (let i = 0; i < +n; i++) {
+        const stat = rollStat();
+        results.push(stat);
+    }
+    return results.join(', ');
+}
+
+// Rolls a die between 1 and the second term in the content
+const rollDice = (content) => {
+	console.log(content);
+	let [command, n] = content.split(' ');
+	n = n || DEFAULTS.DIE_ROLL;
+	return roll(+n);
+};
+
+module.exports = {
+	handleResponse
+};
---
+++ bot.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const config = require('./config.json');
+const express = require('express');
+const { handleResponse } = require('./actions');
+const client = new Discord.Client();
+
+
+// If token is in environment variable or in config file
+const TOKEN = process.env.TOKEN || config.TOKEN;
+const COMMAND = /^!\w+/;
+
+client.on('ready', () => {
+	console.log('Bot is Ready...');
+});
+
+client.on('message', (msg) => {
+	// Is a message not from the bot
+	if (msg.author.id !== client.user.id && msg.content.match(COMMAND)) {
+		const response = handleResponse(msg);
+		if (!!response) {
+			msg.reply(response);
+		}
+	}
+});
+
+client.login(TOKEN);
+
+// Expose server so can be pinged to keep alive, i am cheap :)
+if (process.env.ISREP === 'true') {
+    const PORT = process.env.PORT || 8080;
+    const app = express();
+    app.get('/', (req, res) => res.send('...'));
+    app.listen(PORT);
+}
